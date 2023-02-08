5e5ccf6f259bea5fc7493585a857a127c81c08df
Kelson Roberto <105894520+Kelsonroberto@users.noreply.github.com>
2023-02-08 18:48:19 -0300

    Add files via upload

---
+++ Desafio notion/asserts/script.js
@@ -0,0 +1,42 @@
+
+function changeIcon1(){
+        let icon = document.getElementById("icon_vazio1")
+        if(icon.src.match('./asserts/img/heart.svg')){
+            icon.src = "./asserts/img/red_heart.png";
+        }
+        else{
+            icon.src = "./asserts/img/heart.svg"
+        }
+}
+
+function changeIcon2(){
+        let icon = document.getElementById("icon_vazio2")
+        if(icon.src.match('./asserts/img/heart.svg')){
+            icon.src = "./asserts/img/red_heart.png";
+        }
+        else{
+            icon.src = "./asserts/img/heart.svg"
+        }
+    }
+
+function changeIcon3(){
+    let icon = document.getElementById("icon_vazio3")
+    if(icon.src.match('./asserts/img/heart.svg')){
+        icon.src = "./asserts/img/red_heart.png";
+    }
+    else{
+        icon.src = "./asserts/img/heart.svg"
+    }
+}
+
+function changeIcon4(){
+        let icon = document.getElementById("icon_vazio4")
+        if(icon.src.match('./asserts/img/heart.svg')){
+            icon.src = "./asserts/img/red_heart.png";
+        }
+        else{
+            icon.src = "./asserts/img/heart.svg"
+        }
+    }
+
+
